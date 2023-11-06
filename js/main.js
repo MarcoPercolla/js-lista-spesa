5054c9f8b4f8d83b673d4526a4df45931e3be45e
ReDMallard <marco@titori.it>
2023-11-06 02:50:56 +0100

    aggiunge pulsante aggiungi senza validazioni

--- js/main.js
+++ js/main.js
@@ -1,3 +1,4 @@
+let itemInput = document.getElementById("groceries");
 const listaSpesa = [
     "pane",
     "pasta",
@@ -6,13 +7,34 @@ const listaSpesa = [
     "carta igenica"
 ];
 
+
+document.getElementById("adder").addEventListener("click", function () {
+    let item = itemInput.value;
+    console.log(item);
+    listaSpesa.push(item);
+    console.log(listaSpesa.length);
+
+    let i = 0;
+    let listElement = "";
+    while (i < listaSpesa.length) {
+        //while (listaSpesa[i]) {
+        const listItem = listaSpesa[i];
+        listElement += `<li>${listItem}</li>`;
+        i++;
+    };
+
+    document.getElementById("lista").innerHTML = listElement;
+
+});
+
+
 let i = 0;
 let listElement = "";
-// while (i < listaSpesa.length) {
-    while (listaSpesa[i]) {
-    const listItem =  listaSpesa[i];
+while (i < listaSpesa.length) {
+    //while (listaSpesa[i]) {
+    const listItem = listaSpesa[i];
     listElement += `<li>${listItem}</li>`;
     i++;
-}
+};
 
 document.getElementById("lista").innerHTML = listElement;
